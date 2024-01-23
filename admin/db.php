a4571dbf773ee54405ca0973db6b94d9b9c059ca
Lingam02 <lingamoorthik@gmail.com>
2024-01-23 16:51:22 +0530

    databases changed

--- admin/db.php
+++ admin/db.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysqli_connect("localhost","root","","sourcecodester_hoteldb") or die(mysql_error());
+$con = mysqli_connect("localhost","u296169589_hotel","Pandu@777","u296169589_hotel") or die(mysqli_error($con ));
 
 ?>
